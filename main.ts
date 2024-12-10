e335b8149724ac15dd6469934bf78457f23fbf9e
t-maru_asob <74588783+t-maru-asobism@users.noreply.github.com>
2024-12-10 17:49:11 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -323,8 +323,8 @@ export enum enMusic {
         let temp: boolean = false;
 //		pins.setPull(DigitalPin.P13, PinPullMode.PullNone);
 //		pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
-		pins.setPull(DigitalPin.P13, PinPullMode.PullDown);
-		pins.setPull(DigitalPin.P14, PinPullMode.PullDown);
+		pins.setPull(DigitalPin.P13, PinPullMode.PullUp);
+		pins.setPull(DigitalPin.P14, PinPullMode.PullUp);
         switch (direct) {
             case enPos.LeftState: {
                 if (pins.digitalReadPin(DigitalPin.P13) == value) {              
